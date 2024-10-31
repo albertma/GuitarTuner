0d7779836b2afc80014716b3a17ceb88304acb42
albertma <albert.ma@foxmail.com>
2024-10-31 14:34:20 +0800

    Initial Commit

---
+++ GuitarTunerTests/GuitarTunerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerTests.swift
+//  GuitarTunerTests
+//
+//  Created by albertma on 2024/10/31.
+//
+
+import Testing
+@testable import GuitarTuner
+
+struct GuitarTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
